87e6ded2d1ecbc8aa323d341c58377a685c4df49
EliasDev <eliasbonnin99@gmail.com>
2025-04-10 18:15:32 -0300

    Reto 00 Phyton y Arreglo de Archivos

---
+++ Phyton Elias/Retos y Ejercicios/#00 syntaxis, variables, tipo de dato y hola mundo.py
@@ -0,0 +1,35 @@
+#Ejercicio 00
+
+# En el siguiente URL https://docs.python.org/3/ Tenemos la documentacion de phyton y donde podemos ver todas las funciones y metodos que podemos usar en phyton.
+
+# Hola mundo Python
+
+print("Hola, Phython")
+
+#Declarando variables
+
+my_string = "Esto es una cadena de texto"
+print(my_string)
+
+my_int = 7
+print(my_int) # Mostramos el valor my_int en pantalla.
+
+my_float = 3.14
+print(my_float) # Mostramos el valor my_float en pantalla.
+
+my_bool = False
+print(my_bool) # Mostramos el valor my_bool en pantalla.
+
+MY_CONSTAT = 1
+print(MY_CONSTAT) # Mostramos el valor MY_CONSTAT en pantalla.
+
+'''
+Esto es un comentario de otro tipo.
+
+'''
+
+"""
+Esto es otra forma de comentar.
+
+"""
+
