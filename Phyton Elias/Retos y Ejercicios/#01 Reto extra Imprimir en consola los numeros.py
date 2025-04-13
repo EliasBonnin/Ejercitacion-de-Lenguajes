d1b5f12615f2079db9b03cd4e29f67e332dae783
EliasDev <eliasbonnin99@gmail.com>
2025-04-12 22:09:52 -0300

    Terminamos el Ejercicio 01 extra

---
+++ Phyton Elias/Retos y Ejercicios/#01 Reto extra Imprimir en consola los numeros.py
@@ -0,0 +1,9 @@
+#Ejercicio 01* Extra
+
+# En el siguiente URL https:/python.org Tenemos la documentacion de phyton y donde podemos ver todas las funciones y metodos que podemos usar en phyton.
+ 
+
+for i  in range(10, 56):
+    if (i % 2 == 0 and i != 16 and i % 3 != 0) or i == 55:
+        print(i)
+    
