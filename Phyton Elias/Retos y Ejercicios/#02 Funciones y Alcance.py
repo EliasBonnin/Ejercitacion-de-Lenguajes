63c2e7e59432fbdd2d94de0eeb0fc48bffc78261
EliasDev <eliasbonnin99@gmail.com>
2025-04-15 14:46:16 -0300

    Funciones hechas por el dev

--- Phyton Elias/Retos y Ejercicios/#02 Funciones y Alcance.py
+++ Phyton Elias/Retos y Ejercicios/#02 Funciones y Alcance.py
@@ -4,3 +4,30 @@
  
 # Los retos se encuentran en https://retosdeprogramacion.com/ 
 
+#Funciones
+
+parametro = 10
+
+funcion_variable = 0
+
+funcion_variable2 = "string"
+
+
+def my_funcion ():
+    print("Mi funcion sin retorno")
+
+def my_funcion_con_parametro (parametro):
+    my_variable = 5
+    my_variable = my_variable + parametro
+    return parametro
+
+
+#Llamado a funciones
+
+my_funcion()
+
+variable = my_funcion_con_parametro(parametro)
+print(f"Retorno de funcion con parametro {variable}")
+
+
+
