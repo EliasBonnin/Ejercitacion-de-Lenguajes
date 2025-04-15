58ab9527b648a4fa67449fc4220d4df7e95328b0
EliasDev <eliasbonnin99@gmail.com>
2025-04-15 15:53:10 -0300

    Terminamos la primera parte del ejercicio 01

--- Phyton Elias/Retos y Ejercicios/#02 Funciones y Alcance.py
+++ Phyton Elias/Retos y Ejercicios/#02 Funciones y Alcance.py
@@ -49,5 +49,41 @@ doble = lambda x: x * 2 # Guardamos una "funcion" matematica en este ejemplo par
 print(doble(4))
 
 sumar = lambda a, b: a + b # Mas de un parametro
-sumar(print(2, 3))
+sumar_variable = sumar(2, 3) 
+print(sumar_variable)
 
+print("funcion print") # Funcion print
+
+print(f"imprimimos el tipo usando type {type(sumar)}") # Funcion Type
+
+print(f"Devolvemos la longitud de la variable usando len = {len(funcion_variable2)}") # Funcion Len
+
+range_list= list(range(1,5))
+
+my_list=list(range(6, 10))
+
+print(f"Generamos un rango de numeros usando range y list {range_list}") # Genera un rango de numeros en una lista
+
+nombres = ['Elias', 'Yoshi', 'Rodri']
+edad = [26 , 23, 23]
+
+zip_list = zip(nombres, edad)
+print(f"unificamos 2 listas diferentes utilizando zip {list(zip_list)}") # Utilizamos 2 listas distinitas y las unificamos
+
+round_num =  3.3
+print(f"Hacemos un redondeo utilizando round  de 3.3 a {round(round_num)}")
+
+abs_num = -4
+print(f"Utilizando la funcion abs obtenemos el valor absoluto de un numero de -4 a {abs(abs_num)}") # Utilizamos abs para obtener el absoluto
+
+# Funciones dentro de funciones
+
+var_edad = 26.5
+
+def Array_nombres_edad(edad):
+    round_edad = round(edad)
+    return round_edad
+
+round_edad_print = Array_nombres_edad(var_edad)
+
+print(round_edad_print)
