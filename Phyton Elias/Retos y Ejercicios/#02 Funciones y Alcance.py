62c70b1348d2817a6d543a73148fd3cce1492ca3
EliasDev <eliasbonnin99@gmail.com>
2025-04-15 15:11:14 -0300

    Empezamos con las funciones de python

--- Phyton Elias/Retos y Ejercicios/#02 Funciones y Alcance.py
+++ Phyton Elias/Retos y Ejercicios/#02 Funciones y Alcance.py
@@ -6,28 +6,48 @@
 
 #Funciones
 
-parametro = 10
+my_param = 10
+salida_funcion = "str"
+salida_funcion2 = 0
+salida_funcion3 = 0
 
 funcion_variable = 0
+funcion_variable2 = "str"
+funcion_variable3 = 5
+funcion_variable4 = 12
 
-funcion_variable2 = "string"
-
-
-def my_funcion ():
+def my_funcion (): # Funcion sin retorno
     print("Mi funcion sin retorno")
 
-def my_funcion_con_parametro (parametro):
+def my_funcion_con_parametro (my_param): # Funcion con retorno
     my_variable = 5
-    my_variable = my_variable + parametro
-    return parametro
+    my_variable = my_variable + my_param
+    return my_param
+
+def my_funcion_con_muchos_parametros (param_1, param_2, param_3): # Funcion con muchos parametros
+    my_variable1 = 5
+    my_variable2 = 10
+    my_variable3 = "Muchos Parametros"
+    my_variable2 = my_variable1 + param_1 - param_3 / param_2
+    return (my_variable2, my_variable3)
+
 
 
 #Llamado a funciones
 
-my_funcion()
+my_funcion() #Llamado a funcion sin retorno
 
-variable = my_funcion_con_parametro(parametro)
-print(f"Retorno de funcion con parametro {variable}")
+my_variable = my_funcion_con_parametro(my_param) #Llamado a funcion con retorno
+print(f"Retorno de funcion con parametro {my_variable}")
 
+salida_funcion, salida_funcion2 = my_funcion_con_muchos_parametros(funcion_variable, funcion_variable3, funcion_variable4)
+print(f"Retorno de funcion con muchos parametros {salida_funcion} {salida_funcion2}") #Llamado a funcion con muchos parametros de retorno
 
+# Funciones de phyton
+
+doble = lambda x: x * 2 # Guardamos una "funcion" matematica en este ejemplo para usarla y ser llamada en el futuro
+print(doble(4))
+
+sumar = lambda a, b: a + b # Mas de un parametro
+sumar(print(2, 3))
 
