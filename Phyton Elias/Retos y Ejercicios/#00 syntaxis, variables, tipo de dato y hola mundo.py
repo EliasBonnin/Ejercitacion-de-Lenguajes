f1cb4b462af5d63dad7b1a463144ad5394a2935c
EliasDev <eliasbonnin99@gmail.com>
2025-04-15 14:21:45 -0300

    Empezamos el reto 02

--- Phyton Elias/Retos y Ejercicios/#00 syntaxis, variables, tipo de dato y hola mundo.py
+++ Phyton Elias/Retos y Ejercicios/#00 syntaxis, variables, tipo de dato y hola mundo.py
@@ -2,6 +2,8 @@
 
 # En el siguiente URL https:/python.org Tenemos la documentacion de phyton y donde podemos ver todas las funciones y metodos que podemos usar en phyton.
 
+# Los retos se encuentran en https://retosdeprogramacion.com/ 
+
 # Hola mundo Python
 
 print("Hola, Phython")
--- Phyton Elias/Retos y Ejercicios/#02 Funciones y Alcance.py
+++ Phyton Elias/Retos y Ejercicios/#02 Funciones y Alcance.py
@@ -1,10 +1,6 @@
-#Ejercicio 01* Extra
+#Ejercicio 02
 
 # En el siguiente URL https:/python.org Tenemos la documentacion de phyton y donde podemos ver todas las funciones y metodos que podemos usar en phyton.
  
 # Los retos se encuentran en https://retosdeprogramacion.com/ 
 
-for i  in range(10, 56):
-    if (i % 2 == 0 and i != 16 and i % 3 != 0) or i == 55:
-        print(i)
-    
