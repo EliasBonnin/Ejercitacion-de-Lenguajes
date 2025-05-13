2eb51b930bafd80d316704a3159df715ae9bce02
EliasDev <eliasbonnin99@gmail.com>
2025-05-13 16:31:53 -0300

    Empezamos el ejercicio 3 *

--- Phyton Elias/Retos y Ejercicios/#03 Estructura de datos extra.py
+++ Phyton Elias/Retos y Ejercicios/#03 Estructura de datos extra.py
@@ -1,3 +1,65 @@
 # Ejercicio 03* Extra
 # En el siguiente URL https:/python.org Tenemos la documentacion de phyton.
 # Los retos se encuentran en https://retosdeprogramacion.com
+
+import os
+
+# Funciones utiles
+
+
+def limpiar_consola():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+# Variables
+
+
+op_interfaz = 0  # Variable que usaremos para movernos sobre la interfaz principal
+nombre_agenda = "Nombre"  # Nombre del agendado
+num_tel = 0  # Numero de Telefono del agendado
+
+# Listas
+
+my_agenda: dict = {}
+
+# Interfaz - Terminal
+
+
+def mostrar_menu():
+    print("\n Agenda de contantos")
+    print("1. insertar Contacto")
+    print("2. Actualizar Contacto")
+    print("3. Buscar Contacto")
+    print("4. Borrar Contacto")
+    print("5. Salir\n")
+
+
+while True:
+    limpiar_consola()
+    mostrar_menu()
+    op_interfaz = input("Elegir opcion 1-5\n")
+
+    if op_interfaz == "1":
+        limpiar_consola()
+        print("Insertar Contacto\n")
+        nombre_agenda = input("Ingresa el nombre del usuario \n")
+        while True:
+            num_tel = input("Ingrese el numero de telefono \n")
+            if num_tel.isdigit() and len(num_tel) <= 11:
+                my_agenda[nombre_agenda] = num_tel
+                print("El contacto se agrego correctamete")
+                break
+            else:
+                print("El numero debe ser solo numeros y debe tener un maximo de 11 digitos \n")
+
+    elif op_interfaz == "2":
+        print("Vamos a actualizar un contacto")
+    elif op_interfaz == "3":
+        print("Vamos a buscar un contacto")
+    elif op_interfaz == "4":
+        print("Vamos a borrar un contacto")
+    elif op_interfaz == "5":
+        print("Hasta Luego\n")
+        print(f"Agenda de contactos\n {my_agenda}")
+        break
+    else:
+        print("Opcion no valida")
