c1204d36e55f04656414397d8041c2d890851c93
EliasDev <eliasbonnin99@gmail.com>
2025-05-17 15:43:36 -0300

    Terminado el reto extra #03.

--- Phyton Elias/Retos y Ejercicios/#03 Estructura de datos extra.py
+++ Phyton Elias/Retos y Ejercicios/#03 Estructura de datos extra.py
@@ -3,6 +3,7 @@
 # Los retos se encuentran en https://retosdeprogramacion.com
 
 import os
+import time
 
 # Funciones utiles
 
@@ -30,11 +31,12 @@ def mostrar_menu():
     print("2. Actualizar Contacto")
     print("3. Buscar Contacto")
     print("4. Borrar Contacto")
-    print("5. Salir\n")
+    print("5. Mostrar Agenda y Salir\n")
 
 
 # limpiar_consola()
 while True:
+    limpiar_consola()
     mostrar_menu()
     op_interfaz = input("Elegir opcion 1-5\n")
 
@@ -46,35 +48,57 @@ while True:
             num_tel = input("Ingrese el numero de telefono \n")
             if num_tel.isdigit() and len(num_tel) <= 11:
                 my_agenda[nombre_agenda] = num_tel
-                print("El contacto se agrego correctamete")
+                print("El contacto se agrego correctamete...\n")
+                time.sleep(1)
                 break
             else:
                 print("El numero debe ser solo numeros y debe tener un maximo de 11 digitos \n")
 
     elif op_interfaz == "2":
         limpiar_consola()
-        print("Actualizar un contacto\n")
+        print("      Actualizar un contacto\n")
         while True:
-            nombre_agenda = input("Ingrese el Nombre o ingrese 0 para salir\n")
+            nombre_agenda = input("Ingrese el Nombre del contacto a Actualizar o ingrese 0 para salir \n")
             if nombre_agenda in my_agenda:
-                num_tel = input("Ingrese el numero de telefono \n")
+                while True:
+                    num_tel = input("Ingrese el NUEVO numero de telefono \n")
+                    if num_tel.isdigit() and len(num_tel) <= 11:
+                        my_agenda[nombre_agenda] = num_tel
+                        print("El contacto se actualizo correctamete...\n")
+                        time.sleep(1)
+                        break
+                    else:
+                        print("El numero debe ser solo numeros y debe tener un maximo de 11 digitos \n")
                 break
             elif nombre_agenda == "0":
                 break
             else:
-                print("El nombre de contacto no existe, intente nuevamente")
-
+                print("El nombre de contacto no existe, intente nuevamente\n")
+    elif op_interfaz == "3":
+        limpiar_consola()
+        print("         Buscar un contacto")
         while True:
-            if num_tel.isdigit() and len(num_tel) <= 11:
-                my_agenda[nombre_agenda] = num_tel
-                print("El contacto se actualizo correctamete")
+            nombre_agenda = input("Ingrese el Contacto a Buscar o 0 para salir\n")
+            if nombre_agenda in my_agenda:
+                print(f"El Numero de {nombre_agenda} es: {my_agenda[nombre_agenda]}\n\n")
+            elif nombre_agenda == "0":
                 break
             else:
-                print("El numero debe ser solo numeros y debe tener un maximo de 11 digitos \n")
-    elif op_interfaz == "3":
-        print("Vamos a buscar un contacto")
+                print("El usuario buscado no Existe\n\n")
     elif op_interfaz == "4":
-        print("Vamos a borrar un contacto")
+        limpiar_consola()
+        print("      Borrar un contacto")
+        while True:
+            nombre_agenda = input("Ingrese el Nombre del Contacto a Eliminar o Ingrese 0 para Salir\n")
+            if nombre_agenda in my_agenda:
+                del my_agenda[nombre_agenda]
+                print("El Contacto a sido Eliminado Correctamente...\n")
+                time.sleep(1)
+                break
+            elif nombre_agenda == "0":
+                break
+            else:
+                print("El Nombre Ingresado no Existe\n")
     elif op_interfaz == "5":
         print("Hasta Luego\n")
         print(f"Agenda de contactos\n {my_agenda}")
