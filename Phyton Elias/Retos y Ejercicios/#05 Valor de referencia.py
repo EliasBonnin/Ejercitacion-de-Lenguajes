f34dcd092ed9892388cd944eaf8b9e2ddad865d1
EliasDev <eliasbonnin99@gmail.com>
2025-05-19 14:04:51 -0300

    Terminamos con valores de referencia reto 05

---
+++ Phyton Elias/Retos y Ejercicios/#05 Valor de referencia.py
@@ -0,0 +1,47 @@
+# Ejercicio 04* Extra
+# En el siguiente URL https:/python.org Tenemos la documentacion de phyton.
+# Los retos se encuentran en https://retosdeprogramacion.com
+
+# Por valor
+
+a = 10
+b = a
+b += 5
+
+print(a)  # B tiene la misma copia de valor de A pero no hace referencia a su valor A no CAMBIA
+print(b)  # Esto significa que no es un valor por referencia sino valor de copia
+
+lista_noref = [1, 2, 3]
+lista_noref2 = lista_noref.copy()  # Podemos asignar sin necesidad de referencias haciendo una COPIA
+
+lista_noref2.append(4)
+print(lista_noref)
+
+
+def funcion_sumar(valor):
+    valor += 5
+    print("Valor adentro", valor)
+
+
+por_valor = 14
+funcion_sumar(por_valor)
+print("Valor afuera", por_valor)  # Vemos como en la funcion no se modifica el valor original
+
+# Por referencia
+
+
+lista_ref = [1, 2, 3]
+lista_ref2 = lista_ref  # Asignamos por referencia
+
+lista_ref2.remove(3)
+print(lista_ref)  # Estamos cambiando la lista 2 pero el cambio se ve reflejado en la lista 1.
+
+
+def lista_referencia(lst):  # La funcion modifica el objetio recibido ORIGINAL
+    lst.append("Funcion")
+    print("dentro", lst)
+
+
+mi_lista = [1, 2, 3]
+lista_referencia(mi_lista)
+print("Fuera, ", mi_lista)
