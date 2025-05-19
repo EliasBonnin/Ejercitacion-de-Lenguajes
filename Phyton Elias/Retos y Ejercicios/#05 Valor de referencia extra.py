6efa69260636d40e4f70749441e997d5cd5b4f28
EliasDev <eliasbonnin99@gmail.com>
2025-05-19 14:28:16 -0300

    Terminamos el reto 05* extra

--- Phyton Elias/Retos y Ejercicios/#05 Valor de referencia extra.py
+++ Phyton Elias/Retos y Ejercicios/#05 Valor de referencia extra.py
@@ -9,21 +9,35 @@ var_ref2 = 10
 var_fun = 0
 var_fun2 = 0
 
+list_ref = [1, 2, 3]
+list_ref2 = [1, 2, 3, 4]
+list_fun = 0
+list_fun2 = 0
+
 # Programa1
 
 
 def Programa1(var1, var2):
-    var2 = var1
+    temp = var1
     var1 = var2
+    var2 = temp
     return (var1, var2)
 
 
 var_fun, var_fun2 = Programa1(var_ref, var_ref2)
 print(f"Valor Original {var_ref}, {var_ref2}")
-print(f"Valor Funcion {var_fun}, {var_fun2}")
+print(f"Valor Funcion {var_fun}, {var_fun2} \n")
 
 # Programa2
 
 
 def Programa2(var3, var4):
-    return
+    temp = var3
+    var3 = var4
+    var4 = temp
+    return (var3, var4)
+
+
+list_fun, list_fun2 = Programa2(list_ref, list_ref2)
+print(f"Referencia Original {list_ref}, {list_ref2}")
+print(f"Referencia Funcion {list_fun}, {list_fun2}")
