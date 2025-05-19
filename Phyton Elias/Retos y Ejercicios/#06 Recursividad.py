dee473ad4140683c1f7844ef193be818386660f2
EliasDev <eliasbonnin99@gmail.com>
2025-05-19 14:37:09 -0300

    Terminamos la recursividad

---
+++ Phyton Elias/Retos y Ejercicios/#06 Recursividad.py
@@ -0,0 +1,16 @@
+# Ejercicio 04* Extra
+# En el siguiente URL https:/python.org Tenemos la documentacion de phyton.
+# Los retos se encuentran en https://retosdeprogramacion.
+
+Numero = 100
+
+
+def Conteo_Numero(valor):
+    if valor < 0 or valor == 0:
+        print("Termina la recursividad en ", valor)
+    else:
+        print(valor)
+        Conteo_Numero(valor - 1)
+
+
+Conteo_Numero(Numero)
