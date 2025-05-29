71d90dcf658e74edec4d5aeef88620f52c3c0534
EliasDev <eliasbonnin99@gmail.com>
2025-05-29 15:45:45 -0300

    terminamos el reto 15 y seguimos con el extra

--- Phyton Elias/Retos y Ejercicios/#15 Asincronia.py
+++ Phyton Elias/Retos y Ejercicios/#15 Asincronia.py
@@ -3,3 +3,16 @@
 # # Los retos se encuentran en https://retosdeprogramacion.com
 
 # Asincronia
+
+from datetime import datetime
+import time
+import asyncio
+
+
+async def tarea(nombre: str, duracion: int):
+    print(f"Tarea: {nombre}, Duracion: {duracion}s, Inicio a {datetime.now()}")
+    time.sleep(duracion)
+    print(f"Tarea: {nombre}, Fin: {datetime.now()}")
+
+asyncio.run(tarea("1", 2))
+asyncio.run(tarea("2", 4))
