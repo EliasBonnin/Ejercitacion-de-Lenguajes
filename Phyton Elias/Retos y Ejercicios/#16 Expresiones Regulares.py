ccd3ed2b4dcdad3d3040d3a466c67337a6cc90b7
EliasDev <eliasbonnin99@gmail.com>
2025-06-05 13:24:48 -0300

    terminamos el ejercicio 16

--- Phyton Elias/Retos y Ejercicios/#16 Expresiones Regulares.py
+++ Phyton Elias/Retos y Ejercicios/#16 Expresiones Regulares.py
@@ -6,7 +6,30 @@ import re
 
 
 def encontrar_numeros(texto: str) -> list:
-    return re.findall(r"\d+", texto)
+    return re.findall(r"\d+", texto)  # Busca si existe uno o mas digitos en un texto
 
 
 print(encontrar_numeros("El ejercicio de la fecha de hoy 06/06/25"))
+
+# Extra
+
+
+def validate_email(email: str) -> bool:
+    return bool(re.match(r"^[\w.+-]+@[\w]+\.[a-zA-Z]+$", email))
+
+
+print(validate_email("elias@gmail.com"))
+
+
+def validate_phone(phone: str) -> bool:
+    return bool(re.match(r"^\+?[\d\s]{3,}$", phone))
+
+
+print(validate_phone("+54 1554334"))
+
+
+def validate_url(url: str) -> bool:
+    return bool(re.match(r"^http[s]?://(www.)?[\w]+\.[a-zA-Z]{2,}$", url))
+
+
+print(validate_url("http://www.elias.dev"))
