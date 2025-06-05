6482398c3194a34478818d6939bfe09fb41eb400
EliasDev <eliasbonnin99@gmail.com>
2025-06-05 15:01:07 -0300

    terminamos el reto 18

--- Phyton/Retos y Ejercicios/#18 Conjuntos.py
+++ Phyton/Retos y Ejercicios/#18 Conjuntos.py
@@ -2,7 +2,7 @@
 # En el siguiente URL https:/python.org Tenemos la documentacion de phyton
 # # Los retos se encuentran en https://retosdeprogramacion.com
 
-# Conjuntos
+# Conjuntos (Lista)
 
 conjunto = [1, 2, 3, 4, 5]
 
@@ -30,10 +30,24 @@ conjunto[3] = 3
 
 print(conjunto)
 
-print(f"el elemnto existe? {-1 in conjunto}")
+print(f"el elemento existe? {-1 in conjunto}")
 
 # del conjunto   Esto elimina el objeto entero del programa a tener en cuenta
 
 conjunto.clear()  # Se limpia completamente la estructura de datos
 
 print(conjunto)
+
+# Extra
+
+set_1 = {1, 2, 3, 4, 5, 8}
+set_2 = {1, 2, 3, 4, 6, 7}
+
+print(f"Union: {set_1.union(set_2)}")
+
+print(f"Interseccion: {set_1.intersection(set_2)}")
+
+print(f"Diferencia: {set_1.difference(set_2)}")
+print(f"Diferencia: {set_2.difference(set_1)}")
+
+print(f"Diferencia simetrica: {set_1.symmetric_difference(set_2)}")
