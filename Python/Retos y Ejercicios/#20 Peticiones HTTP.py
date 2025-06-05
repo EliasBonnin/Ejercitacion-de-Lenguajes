c5dcf736a0d2270e0d4c99dab4ada305372dc0b4
EliasDev <eliasbonnin99@gmail.com>
2025-06-05 17:00:42 -0300

    Avance en el ejercicio 20 extra

--- Python/Retos y Ejercicios/#20 Peticiones HTTP.py
+++ Python/Retos y Ejercicios/#20 Peticiones HTTP.py
@@ -3,10 +3,37 @@
 # # Los retos se encuentran en https://retosdeprogramacion.com
 
 import requests
+import time
 
 respuesta = requests.get("https://google.com/")
 
 if respuesta.status_code == 200:
-    print(respuesta.text)
+    print(respuesta)
 else:
     print(f"No se obtuvo la respuesta exitosa codigo: {respuesta.status_code}")
+
+# Extra
+
+
+def MostrarMenu():
+    print("Busca tu Pokemon")
+    print("1. Buscar pokemon")
+    print("2. Salir\n")
+
+
+while True:
+    MostrarMenu()
+
+    action = input("Elegir Opcion: ")
+
+    if action == "1":
+        pokemon = input("Ingrese el numero o nombre del pokemon: ")
+        respuesta = requests.get(f"https://pokeapi.co/api/vs/pokemon/{pokemon}/")
+        if respuesta.status_code == 200:
+            print(respuesta)
+            time.sleep(2)
+        else:
+            print(f"No se obtuvo la respuesta exitosa codigo: {respuesta.status_code}")
+            time.sleep(2)
+    elif action == "2":
+        break
