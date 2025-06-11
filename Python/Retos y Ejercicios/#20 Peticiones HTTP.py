e044c56fbf87db406ea8c22c5108b08c99158a68
EliasDev <eliasbonnin99@gmail.com>
2025-06-11 11:21:23 -0300

    terminamos el ejercicio 20 y su extra

--- Python/Retos y Ejercicios/#20 Peticiones HTTP.py
+++ Python/Retos y Ejercicios/#20 Peticiones HTTP.py
@@ -16,7 +16,7 @@ else:
 
 
 def MostrarMenu():
-    print("Busca tu Pokemon")
+    print("\nBusca tu Pokemon")
     print("1. Buscar pokemon")
     print("2. Salir\n")
 
@@ -27,11 +27,44 @@ while True:
     action = input("Elegir Opcion: ")
 
     if action == "1":
-        pokemon = input("Ingrese el numero o nombre del pokemon: ")
-        respuesta = requests.get(f"https://pokeapi.co/api/vs/pokemon/{pokemon}/")
+        pokemon = input("Ingrese el numero o nombre del pokemon: ").lower()
+        respuesta = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}/")
         if respuesta.status_code == 200:
-            print(respuesta)
-            time.sleep(2)
+            datos = respuesta.json()
+            print("\nPokemon Encontrado!!")
+            print(f"Nombre: {datos["name"]}")
+            print(f"ID: {datos["id"]}")
+            print(f"Peso: {datos["weight"]}")
+            print(f"Altura: {datos["height"]}")
+            print("Tipo(s):")
+            for tipo in datos["types"]:
+                print(tipo["type"]["name"])
+            print("Juegos!")
+            for juego in datos["game_indices"]:
+                print(juego["version"]["name"])
+
+            respuesta = requests.get(f"https://pokeapi.co/api/v2/pokemon-species/{pokemon}/")
+            if respuesta.status_code == 200:
+                url = respuesta.json()["evolution_chain"]["url"]
+                respuesta = requests.get(url)
+                if respuesta.status_code == 200:
+                    datos = respuesta.json()
+                    print("Cadena de evolucion: ")
+
+                    def get_evoluciones(datos):
+                        print(datos["species"]["name"])
+                        if "evolves_to" in datos:
+                            for evolucion in datos["evolves_to"]:
+                                get_evoluciones(evolucion)
+
+                    get_evoluciones(datos["chain"])
+
+                else:
+                    print(f"No se obtuvo la respuesta exitosa a evoluciones codigo: {respuesta.status_code}")
+            else:
+                print(f"No se obtuvo la respuesta exitosa a evoluciones codigo: {respuesta.status_code}")
+
+            time.sleep(3)
         else:
             print(f"No se obtuvo la respuesta exitosa codigo: {respuesta.status_code}")
             time.sleep(2)
