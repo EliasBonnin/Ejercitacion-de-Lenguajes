1f7bc619e9cd32f9525bcf8f4eba534466c7bccb
EliasDev <eliasbonnin99@gmail.com>
2025-06-12 15:19:16 -0300

    terminamos el ejercicio 21

--- Python/Retos y Ejercicios/#21 Callbacks.py
+++ Python/Retos y Ejercicios/#21 Callbacks.py
@@ -2,6 +2,10 @@
 # En el siguiente URL https:/python.org Tenemos la documentacion de phyton
 # # Los retos se encuentran en https://retosdeprogramacion.com
 
+import random
+import time
+
+
 def proceso_saludo(nombre: str, callback):
     callback(nombre)
 
@@ -11,3 +15,28 @@ def saludo_callback(nombre: str):
 
 
 proceso_saludo("Elias", saludo_callback)
+
+
+# Extra
+
+def procesar_pedido(nombre_comida: str, confirmar_callback, terminado_callback, enviado_callback):
+    confirmar_callback(nombre_comida)
+    time.sleep(random.randint(1, 10))
+    terminado_callback(nombre_comida)
+    time.sleep(random.randint(1, 10))
+    enviado_callback(nombre_comida)
+
+
+def proceso_confirmar(nombre_comida: str):
+    print(f"Tu pedido {nombre_comida} a sido confirmado")
+
+
+def pedido_terminado(nombre_comida: str,):
+    print(f"Tu pedido {nombre_comida} esta listo")
+
+
+def pedido_enviado(nombre_comida: str,):
+    print(f"Tu pedido {nombre_comida} a sido enviado")
+
+
+procesar_pedido("Pizza triple queso", proceso_confirmar, pedido_terminado, pedido_enviado)
