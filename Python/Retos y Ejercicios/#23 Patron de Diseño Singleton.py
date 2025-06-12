5ce5ab49af5a2e90cfcc7af798541243846e6e43
EliasDev <eliasbonnin99@gmail.com>
2025-06-12 16:39:42 -0300

    terminamos el reto 23 con extra

--- Python/Retos y Ejercicios/#23 Patron de Diseño Singleton.py
+++ Python/Retos y Ejercicios/#23 Patron de Diseño Singleton.py
@@ -17,3 +17,49 @@ singletone = Singleton()
 print(singletone)
 singletone1 = Singleton()
 print(singletone1)
+
+# Extra
+
+
+class Usuario_Sesion:
+
+    instancia = None
+
+    id = str = None
+    nombre_usuario = str = None
+    nombre = str = None
+    email = str = None
+
+    def __new__(cls):
+        if not cls.instancia:
+            cls.instancia = super(Usuario_Sesion, cls).__new__(cls)
+
+        return cls.instancia
+
+    def set_usuario(self, id, nombre_usuario, nombre, email):
+        self.id = id
+        self.nombre_usuario = nombre_usuario
+        self.nombre = nombre
+        self.email = email
+
+    def get_usuario(self):
+        return print(f"{self.id}, {self.nombre_usuario}, {self.nombre}, {self.email}")
+
+    def borrar_usuario(self):
+        self.id = None
+        self.nombre_usuario = None
+        self.nombre = None
+        self.email = None
+
+
+Sesion1 = Usuario_Sesion()
+Sesion1.set_usuario(1, "pedrito", "elias", "carlostrinidad@gmail.com")
+print(Sesion1.get_usuario())
+
+
+Sesion2 = Usuario_Sesion()
+print(Sesion2.get_usuario())
+
+Sesion3 = Usuario_Sesion()
+print(Sesion3.borrar_usuario())
+print(Sesion3.get_usuario())
