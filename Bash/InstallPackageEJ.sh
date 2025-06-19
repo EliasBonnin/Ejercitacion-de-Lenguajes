a46584a90a1e7ee236aecf99f4c2019eae71c09b
EliasPC <eliasbonnin99@gmail.com>
2025-06-19 16:55:27 -0300

    terminamos el ejemplo de instalacion de package

--- Bash/InstallPackageEJ.sh
+++ Bash/InstallPackageEJ.sh
@@ -4,3 +4,18 @@
 # Instalar Package "htop"
 
 #!/bin/bash
+
+comando=htop #Path de htop si estuviera instalado
+
+if command -v $comando # Verificamos si esta instalado htop
+then
+    echo "$comando existe!"
+else
+    echo "$comando no existe, instalandolo..."
+    sudo apt update && sudo apt install -y $comando  
+fi
+
+$comando
+
+    # && Anida los comandos
+    # -y No me preguntes si quiero o no solo hacelo
