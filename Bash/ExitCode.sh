b6eea00e105484d9072b473b1938d9a38c09e065
EliasPC <eliasbonnin99@gmail.com>
2025-06-23 13:29:06 -0300

    Terminamos un ejemplo de exitcode

--- Bash/ExitCode.sh
+++ Bash/ExitCode.sh
@@ -5,3 +5,15 @@
 
 #!/bin/bash
 
+package=htop
+
+sudo apt install $package  >> package_install_results.log
+
+if [ $? -eq 0 ]
+then
+    echo "La instalacion de $package fue exitosa"
+    echo "Nuevo comando:"
+    which $package
+else
+    echo "La instalacon de $package no fue exitosa"
+fi 
