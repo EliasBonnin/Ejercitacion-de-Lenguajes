e6487d7e4e82efb7f257a6bb372e077933d6625c
EliasPC <eliasbonnin99@gmail.com>
2025-06-23 15:30:31 -0300

    Terminamos el ejercicio forloops

--- Bash/ForLoops.sh
+++ Bash/ForLoops.sh
@@ -4,3 +4,19 @@
 # For Loops
 
 #!/bin/bash
+
+for i in {1..5}
+do
+    echo $i
+    sleep 0.5
+done
+
+echo "Se termino el loop for, estamos afuera"
+sleep 0.5
+clear
+
+for file in ~/logfiles/*.log  # Busca los archivos en logiles, y que estos terminen .log
+do
+    tar -czvf $file.tar.gz $file # crea un file compressed 
+done
+
