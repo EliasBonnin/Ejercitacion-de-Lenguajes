825e3b8f212cdc81c84556f5e2dfe8dcc2f68d0e
EliasDev <eliasbonnin99@gmail.com>
2025-06-23 19:29:34 -0300

    terminamos la primera parte de el reto 24

--- Python/Retos y Ejercicios/#24 Patrones de Diseño Decoradores.py
+++ Python/Retos y Ejercicios/#24 Patrones de Diseño Decoradores.py
@@ -3,3 +3,29 @@
 # # Los retos se encuentran en https://retosdeprogramacion.com
 
 # Decoradores
+
+def imprimir_funcion(funcion):
+    def print_funcion():
+        print(f"La funcion {funcion.__name__} ha sido llamada")
+        return funcion
+    return print_funcion
+
+
+@imprimir_funcion
+def ejemplo_funcion():
+    pass
+
+
+@imprimir_funcion
+def ejemplo_funcion2():
+    pass
+
+
+@imprimir_funcion
+def ejemplo_funcion3():
+    pass
+
+
+ejemplo_funcion()
+ejemplo_funcion2()
+ejemplo_funcion3()
