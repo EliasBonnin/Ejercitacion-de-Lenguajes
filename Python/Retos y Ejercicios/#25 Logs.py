a3fd388997b3838e33beb101df57e8e42e628319
EliasDev <eliasbonnin99@gmail.com>
2025-06-24 10:48:18 -0300

    Terminamos la primera parte del reto 25

--- Python/Retos y Ejercicios/#25 Logs.py
+++ Python/Retos y Ejercicios/#25 Logs.py
@@ -3,3 +3,19 @@
 # # Los retos se encuentran en https://retosdeprogramacion.com
 
 # Logs
+
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s ",
+    handlers=[logging.StreamHandler()])
+
+logging.debug("Mensaje de DEBUG")
+
+logging.info("Mensaje de informacion (INFO)")  # Esta funcionando correctamente
+
+logging.warning("Mensaje de WARNING")  # Algo que no esperabamos sucedio
+
+logging.error("Esto es un ERROR")  # Indica un error mas grave
+
+logging.critical("Esto es un error CRITICAL")  # El error es demasiado grave
