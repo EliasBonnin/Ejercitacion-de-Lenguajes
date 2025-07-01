1313f3e9d857839b820ebf89c482e65ed9d4b04e
EliasDev <eliasbonnin99@gmail.com>
2025-07-01 16:11:48 -0300

    Terminamos el reto 28

--- Python/Retos y Ejercicios/#28 Principio de Sustitcion de Liskov.py
+++ Python/Retos y Ejercicios/#28 Principio de Sustitcion de Liskov.py
@@ -37,3 +37,66 @@ pajaro = Ave()
 pajaro.move()
 pollito = Chicken()
 pollito.move()
+
+# Extra
+
+
+class Vehiculo:
+
+    def __init__(self, velocidad=0):
+        self.velocidad = velocidad
+
+    def acelerar(self, incremento):
+        self.velocidad += incremento
+        print(f"Velocidad:  {self.velocidad} Km/h")
+
+    def frenar(self, decremento):
+        self.velocidad -= decremento
+        if self.velocidad <= 0:
+            self.velocidad = 0
+        print(f"Velocidad:  {self.velocidad} Km/h")
+
+
+class Auto(Vehiculo):
+    def acelerar(self, incremento):
+        print("El Auto esta acelerando")
+        super().acelerar(incremento)
+
+    def frenar(self, decremento):
+        print("El Auto esta frenando")
+        super().frenar(decremento)
+
+
+class Moto(Vehiculo):
+
+    def acelerar(self, incremento):
+        print("La moto esta acelerando")
+        super().acelerar(incremento)
+
+    def frenar(self, decremento):
+        print("La moto esta frenando")
+        super().frenar(decremento)
+
+
+class Bicicleta(Vehiculo):
+    def acelerar(self, incremento):
+        print("La bicicleta esta acelerando")
+        super().acelerar(incremento)
+
+    def frenar(self, decremento):
+        print("La bicicleta esta frenando")
+        super().frenar(decremento)
+
+
+def test_vehiculo(vehiculo):
+    vehiculo.acelerar(10)
+    vehiculo.frenar(5)
+
+
+auto = Auto()
+bici = Bicicleta()
+moto = Moto()
+
+test_vehiculo(auto)
+test_vehiculo(bici)
+test_vehiculo(moto)
