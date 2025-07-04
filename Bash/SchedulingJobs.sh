4dad589a67f7a38c3b8320708745a22401200df5
EliasPC <eliasbonnin99@gmail.com>
2025-07-04 14:41:47 -0300

    Estudiamos el caso de at, una manera de scheduling jobs no tan convencional

--- Bash/SchedulingJobs.sh
+++ Bash/SchedulingJobs.sh
@@ -3,3 +3,19 @@
 
 # Scheduling Jobs
 
+#!/bin/bash
+
+logfile=job_results.log
+
+echo "The script se ejecuto en esta Fecha y Hora $(date)" > $logfile
+
+# Se puede ejecutar usando el comando at
+# Ejemplo
+    # at 15:30 -y -f ./SchedulingJobs
+    # Este comando se ejecutara al hora establecida ya que no le especificamos una fecha.
+    # at 15:30 081624 -y -f ./SchedulingJobs (Mes-Dia-Anio)
+    # Podemos agregar una fecha para los comandos se ejecuten en esa fecha.
+# Con el comando "atq" Podemos ver la lista de comandos para ejecutarse.
+# Con el comando "atrm (Numero de referncia)" podemos remover los comandos.
+
+rm $logfile
